16a4a1930fecf977292be38dd9f76af170110b80
veee <vinaysaldanha101@gmail.com>
2025-10-12 19:58:32 +0530

    initialized the repository and added vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-black text-white text-3xl">Frontend components coming soon...</div>
+    </>
+  )
+}
+
+export default App
